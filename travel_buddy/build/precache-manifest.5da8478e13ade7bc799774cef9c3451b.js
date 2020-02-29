4a2bc9a7853d8452da4b5469de8f2ff7f2c6066f
Jaafar Rammal <jaafarrammal@Sableur-Mac.local>
2020-02-29 14:06:52 +0000

    some heroku build

---
+++ travel_buddy/build/precache-manifest.5da8478e13ade7bc799774cef9c3451b.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fe1a6decdaecb27afe8bc0dc64bcde75",
+    "url": "/index.html"
+  },
+  {
+    "revision": "697b48473140d51eb196",
+    "url": "/static/css/main.31fc55dc.chunk.css"
+  },
+  {
+    "revision": "61ac2742660a8324ad4b",
+    "url": "/static/js/2.7bf5b71b.chunk.js"
+  },
+  {
+    "revision": "f231859d6585c4cd5f80c344783ed269",
+    "url": "/static/js/2.7bf5b71b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "697b48473140d51eb196",
+    "url": "/static/js/main.35df1e11.chunk.js"
+  },
+  {
+    "revision": "8302e7a7c07bbca78ba3",
+    "url": "/static/js/runtime-main.b5917801.js"
+  },
+  {
+    "revision": "ada9cf93f6d1b4268643e107120aa96b",
+    "url": "/static/media/background.ada9cf93.jpg"
+  },
+  {
+    "revision": "1897f97542abc7ed782d257ee781bc40",
+    "url": "/static/media/user1.1897f975.png"
+  }
+]);
