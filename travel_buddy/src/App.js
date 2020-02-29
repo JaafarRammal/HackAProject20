46e886b52d6c21f5bf90fef906048fccf58459ba
Surani Adrian-Petru <surani@Suranis-MacBook-Air.local>
2020-02-29 10:54:17 +0000

    Initial Commits

--- travel_buddy/src/App.js
+++ travel_buddy/src/App.js
@@ -1,11 +1,24 @@
-import React from 'react';
-import './App.css';
+import React, {useState, useEffect} from 'react';
 import './Animations.css';
+import SearchBar from './SearchBar';
+import './App.css';
+import axios from 'axios';
 
-function App() {
+
+const App = () => {
+  useEffect(() => {
+    const getCards = async () => {
+      let res = await axios.get('http:localhost:3000/profiles');
+
+      console.log(res.data);
+    }
+    getCards();
+  })
+
   return (
-    <div className="App">
-    
+    <div className="">
+        <SearchBar/>
+        <p className = "ph-text">There are 500 hosts found in Lebanon. Choose your favourite one !</p>
     </div>
   );
 }
