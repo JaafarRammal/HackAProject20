11bc2a6471a62fab2c20466b123f6f96f635dd8d
Jaafar Rammal <jaafarrammal@Sableur-Mac.local>
2020-02-29 16:24:11 +0000

    update

---
+++ TravelBuddy/TravelBuddy/Controllers/Details.swift
@@ -0,0 +1,30 @@
+//
+//  Details.swift
+//  TravelBuddy
+//
+//  Created by Jaafar Rammal on 2/29/20.
+//  Copyright © 2020 Jaafar Rammal. All rights reserved.
+//
+
+import UIKit
+
+class Details: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
