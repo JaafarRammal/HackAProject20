efec8e0113b08c09a4cfef382b85845b6f9be592
Surani Adrian-Petru <surani@Suranis-MacBook-Air.local>
2020-02-29 12:12:02 +0000

    Solving CORS

---
+++ backend/index.js
@@ -0,0 +1,14 @@
+const express = require('express')
+const userRouter = require('./routers/profile')
+require('./db/mongoose')
+
+const app = express()
+
+const port = process.env.PORT || 3000
+
+app.use(express.json())
+app.use(userRouter)
+
+app.listen(port, () => {
+    console.log('Server is on port ' + port)
+})
--- travel_buddy/src/App.js
+++ travel_buddy/src/App.js
@@ -9,16 +9,12 @@ import axios from 'axios';
 const App = () => {
   useEffect(() => {
     const getCards = async () => {
-      let res = await axios.get('3.8.56.93:3000/profiles');
+      let res = await axios.get('http://3.8.56.93:3000/profiles');
 
       console.log(res.data);
     }
-    try {
+
     getCards();
-    }
-    catch(e) {
-      console.error(e);
-    }
   },[])
 
   return (
